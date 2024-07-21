6736629d1d83c6def7bfa8ddaa34ff29d99f677d
gitsampleuser2024 <gitsampleuser@gmail.com>
2024-07-21 15:15:39 +0530

    created math util with add and subtract method

---
+++ math_util.py
@@ -0,0 +1,9 @@
+def add(x, y):
+    """Adds two numbers and returns the result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtracts two numbers and returns the result."""
+    return x - y
+
+
