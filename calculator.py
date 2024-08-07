fadb848ba27a81af18f2349abe49641f5be63184
gitsampleuser2024 <gitsampleuser@gmail.com>
2024-08-07 22:31:43 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,14 @@
 def add(x, y):
-    """Adds two numbers"""
-    print(f"Adding {x} and {y}")
+    """Adds two numbers and returns the result."""
     return x + y
+
+def subtract(x, y):
+    """Subtracts two numbers and returns the result."""
+    return x - y
+
+def multiply(x, y):  # New function
+    """Multiplies two numbers and returns the result."""
+    return x * y
+
+def divide(a, b):
+    return a / b
